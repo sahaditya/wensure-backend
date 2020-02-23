d7d949804ecf1d459704cec2aea650b317d1a7e0
manoj_sah <manoj@piktorlabs.com>
2020-02-23 06:19:03 +0530

    add money added

--- routes/api_router.js
+++ routes/api_router.js
@@ -5,7 +5,7 @@ const user_service = require("../service/user_service");
 const policy_service = require("../service/policy_service");
 const ledger = require("../service/ledger_service");
 const login = require("../service/login_service");
-const add_money_service = require("../service/add_money_service");
+const money_service = require("../service/money_service");
 
 router.get("/test", user_service.test);
 
@@ -16,6 +16,7 @@ router.get("/policy", policy_service.get_policy);
 router.post("/ledger/user", ledger.create_user_ledger);
 router.post("/ledger/admin", ledger.create_admin_ledger); //NOT REQUIRED
 router.post("/login", login.login);
-router.post("/addmoney", add_money_service.add_money);
+router.post("/addmoney", money_service.add_money);
+router.get("/balance", money_service.check_balance);
 
 module.exports = router;
---
+++ service/money_service.js
@@ -0,0 +1,88 @@
+const Logger = require("../utils/logger");
+const UserLedger = require("../model/ledger/user_ledger");
+const Policy = require("../model/policy");
+const constant = require("../utils/constants");
+
+get_cash_back = (amount, cback) => {
+  cb = (constant.CASHBACK_PERCENTAGE / 100) * amount + cback;
+  Logger.debug(cb);
+  return cb;
+};
+exports.add_money = async (req, res) => {
+  try {
+    Logger.debug("Entered into add_money method");
+    let user_policy_ledger = await UserLedger.find({
+      $and: [
+        { uid: req.body.uid },
+        { "transaction_history.policy_id": req.body.policy_id }
+      ]
+    });
+    const new_payment = {
+      amount: req.body.amount,
+      date: new Date(),
+      type: req.body.type,
+      cashback:
+        req.body.type == constant.TRANSACTION_TYPE[1]
+          ? get_cash_back(user_policy_ledger, req.body.amount) +
+            user_policy_ledger.cashback
+          : user_policy_ledger.cashback
+    };
+    old_obj = JSON.parse(JSON.stringify(user_policy_ledger));
+    delete old_obj.__v;
+    for (let element in old_obj[0].transaction_history) {
+      // old_obj[0].transaction_history.forEach(element => {
+      if (element.policy_id === req.body.policy_id) {
+        const cb = element.total_cashback;
+        new_obj = {
+          amount: req.body.amount,
+          date: req.body.date,
+          status: "SUCCSEED",
+          type: "CREDIT",
+          cashback: 30
+        };
+        element.transactions.push(new_obj);
+        element.total_cashback = element.transactions.reduce((acc, val) => {
+          return acc + val.cashback;
+        }, 0);
+        break;
+      }
+    }
+    let saved = await UserLedger.updateOne(
+      { uid: req.body.uid },
+      { ...old_obj[0] }
+    );
+    res.json(old_obj);
+    res.end();
+    Logger.debug("Exited from add_money method");
+  } catch (error) {
+    res.json({
+      error: true,
+      message: error.message
+    });
+    Logger.debug("Error happen in add_money method");
+    Logger.debug(error);
+    res.end();
+  }
+};
+
+exports.check_balance = (req, res) => {
+  try {
+    const uid = req.query && req.query.uid;
+    const user_ledger = UserLedger.find({ uid: uid });
+    current_balance = user_ledger[0].transaction_history.reduce((acc, val) => {
+      return acc + val.amount;
+    }, 0);
+    res.json({
+      current_balance,
+      status: 400
+    });
+  } catch (error) {
+    res.json({
+      error: true,
+      message: error.message
+    });
+    Logger.debug("Error happen in check_balance method");
+    Logger.debug(error);
+    res.end();
+  }
+};
